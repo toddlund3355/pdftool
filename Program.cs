08b744bb3b4174e5a2690d0ba0948a4db068086d
Todd Lund <todd.lund@mediakind.com>
2023-09-04 14:12:00 -0700

    pdf text extraction tool - creates markdown files - initial checkin

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        using (PdfDocument document = PdfDocument.Open(args[0]))
+        {
+            foreach (Page page in document.GetPages())
+            {
+                IReadOnlyList<Letter> letters = page.Letters;
+                string example = string.Join(string.Empty, letters.Select(x => x.Value));
+
+                IEnumerable<Word> words = page.GetWords();
+
+                StringBuilder builder = new StringBuilder();
+                builder.AppendFormat("# {0}\n\n", args[0]);
+                builder.Append("## Words\n");
+                foreach (var word in words)
+                {
+                    builder.AppendFormat("{0} ", word);
+                }
+                builder.Append("\n");
+                var fullfile = builder.ToString();
+                // Console.Write(fullfile);
+
+                string newfilename = args[0].Replace(".pdf", ".md");
+
+                File.WriteAllText(newfilename, fullfile);
+
+                // IEnumerable<IPdfImage> images = page.GetImages();
+            }
+        }
+    }
+}
